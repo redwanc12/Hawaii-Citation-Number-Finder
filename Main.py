28ace3aced1db5576e4a6181ac7d1aa635004d9f
redwanchowdhury <redwanc12@gmail.com>
2019-05-04 00:53:23 -0700

    Created first working model. Finds any name within ticket range 015895 to 15905

--- Main.py
+++ Main.py
@@ -1,3 +1,20 @@
 from bs4 import BeautifulSoup
 import requests
 
+idNum = 15895
+name = input("enter a name ")
+
+
+while(idNum < 15905):
+
+	page = requests.get("https://etraffic.ehawaii.gov/etraffic/search/1DTI-19-0" + str(idNum))
+	soup = BeautifulSoup(page.text, 'html.parser')
+
+	for each in soup.find_all('dd'):
+		if(('FIRST' not in each.text) and ('1D' not in each.text)):
+			if(each.text == name):
+				print('1DTI-19-0' + str(idNum))
+				break;
+
+	idNum = idNum + 1;
+
