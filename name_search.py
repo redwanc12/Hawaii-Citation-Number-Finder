20111141addd18be72e12ff03b4041581f07436d
redwanchowdhury <redwanc12@gmail.com>
2019-05-05 00:31:03 -0700

    added new functions. find_max does not work.

--- name_search.py
+++ name_search.py
@@ -2,12 +2,29 @@ from bs4 import BeautifulSoup
 import requests
 
 idNum = 15931
+ID_MIN = 10000
+ID_MAX = 99999
 
-def check_blank(link):
+
+def id_to_link (id):
+	#converts an id to a link for that id
+	return("https://etraffic.ehawaii.gov/etraffic/search/1DTI-19-0" + str(id))
+
+
+def blank(link):
+	#checks ia link does not contain a valid ID.
 	page = requests.get(link)
 	soup = BeautifulSoup(page.text, 'html.parser')
 	if('NO RESULTS' in soup.find('strong').text):
 		return True
 	return False
 
-print(check_blank("https://etraffic.ehawaii.gov/etraffic/search/1DTI-19-0" + str(idNum)))
+def find_max():
+	if(blank(id_to_link(ID_MAX))):
+		ID_MAX = ID_MAX/2
+	else:
+		print(ID_MAX)
+
+find_max()
+
+
