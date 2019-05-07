f05c331200d25d472fff16ef7dc740f104ebbb62
redwanchowdhury <redwanc12@gmail.com>
2019-05-06 21:35:45 -1000

    added comments

--- Main.py
+++ Main.py
@@ -10,13 +10,14 @@ idNum = ID_MAX - 70
 
 
 while(idNum <= ID_MAX):
-
+	#Will check every ID in range idNum to ID_MAX
 	page = requests.get(id_to_link(idNum))
 	soup = BeautifulSoup(page.text, 'html.parser')
 
 	for each in soup.find_all('dd'):
 		if(('FIRST' not in each.text) and ('1D' not in each.text)):
 			if(name in each.text):
+				#If name is a match
 				print('1DTI-19-0' + str(idNum))
 				print(each.text)
 				break
